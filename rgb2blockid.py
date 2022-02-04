01111765b35a2344231894bf4875ab4615a8090c
lishuyu <lishuyustevenli@163.com>
2022-02-04 15:40:19 +0800

    feat lie.mp4 done

--- rgb2blockid.py
+++ rgb2blockid.py
@@ -1,111 +1,41 @@
 import json
+import time
+
 import numpy as np
 import threading
 from tqdm import tqdm
 
+
 class matchBlock:
-    def __init__(self):
-        with open("block_color.json", "r") as f:
-            self.dictionary = json.loads(f.read())
-        self.Lock = threading.BoundedSemaphore(10)
-
-    def find(self, rgb, save=False, row=None, col=None):
-        rgb = np.array(rgb)
-        match = self.dictionary[0]
-        error = np.sum(np.abs(np.array(self.dictionary[0]["avg"]) - rgb))
-        for color in self.dictionary:
-            new_error = np.sum(np.abs(np.array(color["avg"]) - rgb))
-            if new_error < error:
-                match = color
-                error = new_error
-        if save:
-            self.pic[row][col] = match
-            self.Lock.release()
-        else:
-            self.Lock.release()
-            return match
-
-    def matchPicture(self, img):
-        """
-        Input an rbg picture, reture block id need be placed
-        :param img: rbg
-        :return: block id need be placed
-        """
-
-        def eachcolprogress(row_t):
-            process_t = []
-
-            for col in range(len(img[0])):
-                self.Lock.acquire()
-                t = threading.Thread(target=self.find, args=(img[row_t][col], True, row_t, col,))
-                t.start()
-                print(f"\r{row_t},{col},{threading.active_count()}", end="\t")
-                process_t.append(t)
-
-            for t in process_t:
-                t.join()
-
-        self.pic = np.zeros((len(img), len(img[0]))).tolist()
-        process = []
-        for row in range(len(img)):
-            t = threading.Thread(target=eachcolprogress, args=(row,))
-            t.start()
-            process.append(t)
-        for t in process:
-            t.join()
-        return self.pic
-
-class matchBlockV2:
     def __init__(self, img_x, img_y):
         self.pic = np.zeros((img_x, img_y)).tolist()
-        # with open("block_color.json", "r") as f:
-        #     self.dictionary = json.loads(f.read())
+        print("Start Load Dictionary")
         with open("dictionary_rgb.json", "r") as f:
             self.rgb = json.loads(f.read())
-            print("Load Dictionary")
-
-        """rgb_array = []
-        for item in self.dictionary:
-            rgb_array.append(item["avg"])
-        rgb_array = np.array(rgb_array)
-        self.DIClength = len(self.dictionary)
-
-        def find(rgb):
-            return self.dictionary[
-                np.argmin(np.sum(np.abs(rgb_array - np.array([rgb for i in range(self.DIClength)])), axis=1))]["id"]
-
-        self.dictionary_rgb = {}
-        progress_list = []
-        for r in range(256):
-            for g in range(256):
-                for b in range(256):
-                    progress_list.append([r, g, b])
-        for value in tqdm(progress_list):
-            self.dictionary_rgb[f"{value[0]}, {value[1]}, {value[2]}"] = find(value)
-
-        with open("dictionary_rgb.json", "w") as f:
-            f.write(json.dumps(self.dictionary_rgb))"""
+            print("Loaded Dictionary")
 
     def matchPicture(self, img):
         progress_list = []
         for row in range(len(img)):
             for col in range(len(img[0])):
                 progress_list.append([row, col])
-        for value in tqdm(progress_list):
+        for value in progress_list:
             self.pic[value[0]][value[1]] = self.find(img[value[0]][value[1]].tolist())
         return self.pic
 
-    def find(self, rgb, save=False, row=None, col=None):
-        if save:
-            self.pic[row][col] = self.rgb[f"{rgb[0]}, {rgb[1]}, {rgb[2]}"]
-        else:
-            return self.rgb[f"{rgb[0]}, {rgb[1]}, {rgb[2]}"]
+    def find(self, rgb):
+        assert type(rgb[0]) == int and type(rgb[1]) == int and type(rgb[2]) == int
+        return self.rgb[f"{rgb[0]},{rgb[1]},{rgb[2]}"]
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    img = plt.imread("sample.jpg")/255
-    m = matchBlock()
+    img = plt.imread("sample.jpg")
+    assert img.shape[2] == 3
+    starttime = time.time()
+    m = matchBlock(img.shape[0], img.shape[1])
+    print(f"Load time: {round(time.time() - starttime, 2)}s")
+    starttime = time.time()
     pic = m.matchPicture(img)
-    with open("save.json", "w") as f:
-        f.write(json.dumps(pic))
+    print(f"Progress time: {round(time.time() - starttime, 2)}s")
+    print(str(pic))
--- sepFromVid.py
+++ sepFromVid.py
@@ -1,7 +1,11 @@
+import threading
 import time
 
 import cv2
 import json
+
+from tqdm import tqdm
+
 import rgb2blockid
 
 
@@ -16,48 +20,56 @@ def input_vid():
     while ret:
         ret, img = cap.read()
         if ret:
-            i+=1
+            i += 1
             print(f"\rGet new pic {i}", end="")
-            imgs.append(cv2.cvtColor(cv2.resize(img, (96, 54)), cv2.COLOR_RGB2BGR) / 255)
+            imgs.append(cv2.cvtColor(cv2.resize(img, (96, 54)), cv2.COLOR_RGB2BGR))
     print(f"\nEnd capture with time {time.time() - starttime}")
     starttime = time.time()
-    m = rgb2blockid.matchBlock()
+    m = rgb2blockid.matchBlock(54, 96)
+
+    def saveCache(pics_COPY):
+        with open("cache.json", "w") as f:
+            f.write(json.dumps(pics_COPY))
+
+    print("Start progressing")
     pics = []
-    for i in range(len(imgs)):
+    for i in tqdm(range(len(imgs))):
         starttime = time.time()
         pics.append(m.matchPicture(imgs[i]))
-        print(f"\n{i + 1}/{len(imgs)}:{round((i + 1) / len(imgs) * 100, 2)}%\t-------------------------{time.time() - starttime}")
-        if i % 10 == 0:
-            with open("cache.json", "w") as f:
-                f.write(json.dumps(pics))
+        # print(f"\n{i + 1}/{len(imgs)}:{round((i + 1) / len(imgs) * 100, 2)}%\t-------------------------{time.time() - starttime}")
+        if i % 1000 == 0:
+            threading.Thread(target=saveCache, args=(pics,)).start()
     with open("happyNewYear.json", "w") as f:
         f.write(json.dumps(pics))
 
 
 def output(prefix=True):
+    print("Start output commands")
     if prefix:
         with open(f"/Users/lishuyu/Downloads/{input()}", "r") as f:
             pics = json.loads(f.read())
     else:
         with open("happyNewYear.json", "r") as f:
             pics = json.loads(f.read())
-    for i in range(len(pics)):
+    print("load pretrain dataset done")
+    print("start writing commands")
+    for i in tqdm(range(len(pics))):
         pic = pics[i]
         s = "# minecraft convert save.json to command lines\n"
         for row in range(len(pic)):
             for col in range(len(pic[0])):
-                id = pic[row][col]['id']
+                id = pic[row][col]
                 s += f"setblock {col} {0} {row} {id}\n"
                 if id == "gravel" or id == "sand":
                     s += f"setblock {col} {-1} {row} stone\n"
         file_path = "/Users/lishuyu/Library/Application Support/minecraft/saves/虚空/datapacks/test/data/custom/functions/"
         with open(file_path + f"pic{i}.mcfunction", "w") as f:
             f.write(s)
-
+    print("start establish schedule and clear order")
     s = ""
     s_cla = ""
     file_path = "/Users/lishuyu/Library/Application Support/minecraft/saves/虚空/datapacks/test/data/custom/functions/"
-    for i in range(len(pics)):
+    for i in tqdm(range(len(pics))):
         s += f"schedule function custom:pic{i} {i + 5}s\n"
         s_cla += f"schedule clear custom:pic{i}\n"
     with open(file_path + f"pic.mcfunction", "w") as f:
@@ -69,5 +81,5 @@ def output(prefix=True):
 if __name__ == '__main__':
     start_time = time.time()
     # input_vid()
-    output()
-    print(time.time() - start_time)
+    output(False)
+    print("time record:", time.time() - start_time)
