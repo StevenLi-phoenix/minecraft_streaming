d90b1e3f4d759287047b3a0533d3d9e480d587c8
lishuyu <lishuyustevenli@163.com>
2022-02-05 17:29:36 +0800

    fix main.py using independent searching args

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import cv2
 import matplotlib.colors as col
 import matplotlib.pyplot as plt
 import numpy as np
+from rgb2blockid import matchBlock
 
 assert path.exists("minecraft/blockstates")
 assert path.exists("minecraft/models/block")
@@ -75,70 +76,19 @@ def update():
         f.write(json.dumps(dictionary))
 
 
-class matchBlock:
-    def __init__(self):
-        with open("block_color.json", "r") as f:
-            self.dictionary = json.loads(f.read())
-        self.Lock = threading.BoundedSemaphore(10)
-
-    def find(self, rgb, save=False, row=None, col=None):
-        rgb = np.array(rgb)
-        match = self.dictionary[0]
-        error = np.sum(np.abs(np.array(self.dictionary[0]["avg"]) - rgb))
-        for color in self.dictionary:
-            new_error = np.sum(np.abs(np.array(color["avg"]) - rgb))
-            if new_error < error:
-                match = color
-                error = new_error
-        if save:
-            self.pic[row][col] = match
-            self.Lock.release()
-        else:
-            self.Lock.release()
-            return match
-
-    def matchPicture(self, img):
-        """
-        Input an rbg picture, reture block id need be placed
-        :param img: rbg
-        :return: block id need be placed
-        """
-
-        def eachcolprogress(row_t):
-            process_t = []
-
-            for col in range(len(img[0])):
-                self.Lock.acquire()
-                t = threading.Thread(target=self.find, args=(img[row_t][col], True, row_t, col,))
-                t.start()
-                # print(f"\r{row_t},{col},{threading.active_count()}", end="\t")
-                process_t.append(t)
-
-            for t in process_t:
-                t.join()
-
-        self.pic = np.zeros((len(img), len(img[0]))).tolist()
-        process = []
-        for row in range(len(img)):
-            t = threading.Thread(target=eachcolprogress, args=(row,))
-            t.start()
-            process.append(t)
-        for t in process:
-            t.join()
-        return self.pic
-
 class stream:
     def __init__(self):
         self.img = None
         threading.Thread(target=self.capStart).start()
-        self.m = matchBlock()
+        self.m = matchBlock(27, 48)
+
     def capStart(self):
         cap = cv2.VideoCapture(1)
         while True:
             ret, img = cap.read()
             if ret:
-                self.img = cv2.cvtColor(cv2.resize(img, (48,27)), cv2.COLOR_RGB2BGR)/255
-            cv2.waitKey(1000)
+                self.img = cv2.cvtColor(cv2.resize(img, (48, 27)), cv2.COLOR_RGB2BGR) / 255
+            cv2.waitKey(100)
 
     def output(self, pic):
         s = "# minecraft convert save.json to command lines\n"
@@ -164,6 +114,7 @@ class stream:
                 continue
             pic = self.m.matchPicture(self.img)
             threading.Thread(target=self.output, args=(pic,)).start()
+
 
 if __name__ == '__main__':
     # update()
